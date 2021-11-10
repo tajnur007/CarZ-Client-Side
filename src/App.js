39041b15dafdc8e6c0485bc7f5bdd9b78ebad2b5
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-10 14:26:42 +0600

    All pages linked

--- src/App.js
+++ src/App.js
@@ -1,10 +1,70 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Home from './pages/home/Home/Home';
+import Header from './pages/shared/Header/Header';
+import Footer from './pages/shared/Footer/Footer';
+import AddProduct from './pages/addProduct/AddProduct/AddProduct';
+import Contact from './pages/contact/Contact/Contact';
+import Dashboard from './pages/dashboard/Dashboard/Dashboard';
+import ExploreCars from './pages/exploreCars/ExploreCars/ExploreCars';
+import Login from './pages/login/Login/Login';
+import MakeAdmin from './pages/makeAdmin/MakeAdmin/MakeAdmin';
+import ManageAllOrders from './pages/manageAllOrders/ManageAllOrders/ManageAllOrders';
+import ManageProducts from './pages/manageProducts/ManageProducts/ManageProducts';
+import MyOrders from './pages/myOrders/MyOrders/MyOrders';
+import Pay from './pages/pay/Pay/Pay';
+import Register from './pages/register/Register/Register';
+import Review from './pages/review/Review/Review';
 
 function App() {
   return (
     <div className="App">
-
+      <Router>
+        <Header />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/add-product">
+            <AddProduct />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/dashboard">
+            <Dashboard />
+          </Route>
+          <Route exact path="/explore-cars">
+            <ExploreCars />
+          </Route>
+          <Route exact path="/login">
+            <Login />
+          </Route>
+          <Route exact path="/make-admin">
+            <MakeAdmin />
+          </Route>
+          <Route exact path="/manage-all-orders">
+            <ManageAllOrders />
+          </Route>
+          <Route exact path="/manage-products">
+            <ManageProducts />
+          </Route>
+          <Route exact path="/my-orders">
+            <MyOrders />
+          </Route>
+          <Route exact path="/pay">
+            <Pay />
+          </Route>
+          <Route exact path="/register">
+            <Register />
+          </Route>
+          <Route exact path="/review">
+            <Review />
+          </Route>
+        </Switch>
+        <Footer />
+      </Router>
     </div>
   );
 }
