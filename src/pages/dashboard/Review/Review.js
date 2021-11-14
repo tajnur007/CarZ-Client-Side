3cdf3d571dde8bcde534d2ef2c7d423186aff849
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-15 01:12:52 +0600

    Review page design complete

--- src/pages/dashboard/Review/Review.js
+++ src/pages/dashboard/Review/Review.js
@@ -2,8 +2,15 @@ import React from 'react';
 
 const Review = () => {
     return (
-        <div>
-
+        <div className="container my-5">
+            <h1 className="txt-primary pb-3">MY REVIEW</h1>
+            <div className="form-floating w-75 mx-auto">
+                <textarea className="form-control" placeholder="Share your experience with us" id="floatingTextarea2" style={{ height: "150px" }} ></textarea>
+                <label for="floatingTextarea2">Share your experience with us</label>
+            </div>
+            <div>
+                <button className="btn-1 my-3 py-2">Submit Review</button>
+            </div>
         </div>
     );
 };
