f186beaa1a3037e52cf06bd7be13a5998cd2e27f
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-14 13:21:14 +0600

    Pay page complete

--- src/pages/pay/Pay/Pay.js
+++ src/pages/pay/Pay/Pay.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 const Pay = () => {
     return (
-        <div>
-
+        <div className="container my-5">
+            <h3 className="mb-3">Hang On!</h3>
+            <h1>Payment system is coming soon...</h1>
         </div>
     );
 };
