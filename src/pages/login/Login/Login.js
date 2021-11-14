05d99bdad3562d7cf0ff542aa7ae5efb1cf6e38e
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-14 12:47:23 +0600

    Login design complete

--- src/pages/login/Login/Login.js
+++ src/pages/login/Login/Login.js
@@ -1,10 +1,69 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import companyLogo from '../../../images/company/carz-logo-lg.png';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGoogle } from '@fortawesome/free-brands-svg-icons';
+import { faSignInAlt } from '@fortawesome/free-solid-svg-icons';
 
 const Login = () => {
+    const formRef = useRef();
+    const emailRef = useRef();
+    const passwordRef = useRef();
+
+
     return (
         <div>
+            <div className="container container--mini my-5">
+                {/* Company Logo  */}
+                <img className="d-block mx-auto mt-5 w-25" src={companyLogo} alt="" />
 
-        </div>
+                {/* Welcome Message  */}
+                <h2 className="my-3"> Sign Into Your Account </h2>
+
+                {/* Login / Signup Form  */}
+                <form className="d-flex justify-content-center" ref={formRef}>
+
+                    <div className="col-12 col-sm-12 col-md-10 col-lg-8 col-xl-6">
+
+                        {/* Email Input  */}
+                        <div className="form-group text-start mx-5 mb-3">
+                            <label htmlFor="user_login">Email</label>
+                            <input type="email" className="form-control" ref={emailRef} placeholder="Email Address" required />
+                        </div>
+
+                        {/* Password Input  */}
+                        <div className="form-group text-start mx-5 mb-3">
+                            <label htmlFor="user_pass">Password</label>
+                            <input type="password" className="form-control" ref={passwordRef} placeholder="Enter Password" required />
+                        </div>
+
+                        {/* Error message  */}
+                        <p className="text-start text-danger mx-5 mb-3">
+
+                        </p>
+
+                        {/* Login / Signup Button  */}
+                        <div className="form-group text-start mx-5">
+                            <button onClick="" type="submit" className="btn-1 w-100 mb-4">
+                                <FontAwesomeIcon icon={faSignInAlt} /> Login
+                            </button>
+                        </div>
+
+                        <p>----- OR -----</p>
+
+                        {/* Google Login Button  */}
+                        <div className="form-group text-start mx-5">
+                            <button onClick="" className="btn-1 py-2 w-100 mb-4">
+                                <FontAwesomeIcon icon={faGoogle} /> Sign in with Google
+                            </button>
+                        </div>
+
+                    </div>
+
+                </form>
+
+            </div >
+
+        </div >
     );
 };
 
