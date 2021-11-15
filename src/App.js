153aad997efc356aa2e2869c224ad7a66fddba46
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-16 01:09:28 +0600

    AuthProvider added

--- src/App.js
+++ src/App.js
@@ -17,55 +17,58 @@ import MakeAdmin from './pages/dashboard/MakeAdmin/MakeAdmin';
 import ManageAllOrders from './pages/dashboard/ManageAllOrders/ManageAllOrders';
 import ManageProducts from './pages/dashboard/ManageProducts/ManageProducts';
 import PrivateRoute from './pages/shared/PrivateRoute/PrivateRoute';
+import AuthProvider from './contexts/AuthProvider';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Header />
-        <Switch>
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <PrivateRoute exact path="/add-product">
-            <AddProduct />
-          </PrivateRoute>
-          <Route exact path="/contact">
-            <Contact />
-          </Route>
-          <PrivateRoute exact path="/dashboard">
-            <Dashboard />
-          </PrivateRoute>
-          <Route exact path="/explore-cars">
-            <ExploreCars />
-          </Route>
-          <Route exact path="/login">
-            <Login />
-          </Route>
-          <PrivateRoute exact path="/make-admin">
-            <MakeAdmin />
-          </PrivateRoute>
-          <PrivateRoute exact path="/manage-all-orders">
-            <ManageAllOrders />
-          </PrivateRoute>
-          <PrivateRoute exact path="/manage-products">
-            <ManageProducts />
-          </PrivateRoute>
-          <PrivateRoute exact path="/my-orders">
-            <MyOrders />
-          </PrivateRoute>
-          <PrivateRoute exact path="/pay">
-            <Pay />
-          </PrivateRoute>
-          <Route exact path="/register">
-            <Register />
-          </Route>
-          <PrivateRoute exact path="/review">
-            <Review />
-          </PrivateRoute>
-        </Switch>
-        <Footer />
-      </Router>
+      <AuthProvider>
+        <Router>
+          <Header />
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <PrivateRoute exact path="/add-product">
+              <AddProduct />
+            </PrivateRoute>
+            <Route exact path="/contact">
+              <Contact />
+            </Route>
+            <PrivateRoute exact path="/dashboard">
+              <Dashboard />
+            </PrivateRoute>
+            <Route exact path="/explore-cars">
+              <ExploreCars />
+            </Route>
+            <Route exact path="/login">
+              <Login />
+            </Route>
+            <PrivateRoute exact path="/make-admin">
+              <MakeAdmin />
+            </PrivateRoute>
+            <PrivateRoute exact path="/manage-all-orders">
+              <ManageAllOrders />
+            </PrivateRoute>
+            <PrivateRoute exact path="/manage-products">
+              <ManageProducts />
+            </PrivateRoute>
+            <PrivateRoute exact path="/my-orders">
+              <MyOrders />
+            </PrivateRoute>
+            <PrivateRoute exact path="/pay">
+              <Pay />
+            </PrivateRoute>
+            <Route exact path="/register">
+              <Register />
+            </Route>
+            <PrivateRoute exact path="/review">
+              <Review />
+            </PrivateRoute>
+          </Switch>
+          <Footer />
+        </Router>
+      </AuthProvider>
     </div>
   );
 }
