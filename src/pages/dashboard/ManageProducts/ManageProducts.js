964a5b94470f7a0bcc3c43e35d33e7adb10193ac
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-15 12:12:46 +0600

    Manage product page design complete

--- src/pages/dashboard/ManageProducts/ManageProducts.js
+++ src/pages/dashboard/ManageProducts/ManageProducts.js
@@ -2,8 +2,26 @@ import React from 'react';
 
 const ManageProducts = () => {
     return (
-        <div>
-
+        <div className="container my-5">
+            <h1 className="txt-primary pb-3">MANAGE PRODUCTS</h1>
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                        <th scope="col">Car Name and Model</th>
+                        <th scope="col">Status</th>
+                        <th scope="col">Actions</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Lambourgini</td>
+                        <td>Available/Not Available</td>
+                        <td>
+                            <button className="btn btn-danger" >Delete Product</button>
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         </div>
     );
 };
