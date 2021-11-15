73a4aab147b9d1ed168dd03d60741440af7f0001
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-15 12:56:12 +0600

    Make admin page design complete

--- src/pages/dashboard/MakeAdmin/MakeAdmin.js
+++ src/pages/dashboard/MakeAdmin/MakeAdmin.js
@@ -2,8 +2,14 @@ import React from 'react';
 
 const MakeAdmin = () => {
     return (
-        <div>
-
+        <div className="container my-5">
+            <h1 className="txt-primary pb-3">MAKE AN ADMIN</h1>
+            <h4 className="text-success">Give an email address to make an admin</h4>
+            <div class="form-floating mb-3 w-50 mx-auto">
+                <input type="email" class="form-control" id="floatingInput" placeholder=" " />
+                <label for="floatingInput">Admin Email</label>
+            </div>
+            <div><button className="btn-1 py-2">Make Admin</button></div>
         </div>
     );
 };
