77a0976b0cc1314a4f7624281ce6dca648e1656c
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-18 03:21:49 +0600

    Side menus conditionaled with isAdmin

--- src/pages/dashboard/Dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard/Dashboard.js
@@ -1,59 +1,101 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Dashboard.css';
 import { Link } from 'react-router-dom';
 import userIcon from '../../../images/others/user-icon.png';
+import useAuth from '../../../hooks/useAuth';
 
 const Dashboard = () => {
+    const { user } = useAuth();
+    const [isAdmin, setIsAdmin] = useState();
+
+    useEffect(() => {
+        const data = { email: user.email };
+        fetch('http://localhost:7007/isAdmin', {
+            method: 'POST',
+            headers: {
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
+            .then(resp => resp.json())
+            .then(data => setIsAdmin(data.isAdmin))
+
+        console.log(isAdmin);
+    }, [isAdmin, user.email])
 
     return (
         <div className="dashboard-container d-flex">
+            {/* Side Menu  */}
             <div className="col-3 p-3 pt-5 side-menu border-end">
-                <p className="dashboard-item">
-                    <Link className="dashboard-item-link" to="/pay" >
-                        Pay
-                    </Link>
-                </p>
-                <p className="dashboard-item">
-                    <Link className="dashboard-item-link" to="/my-orders" >
-                        My Orders
-                    </Link>
-                </p>
-                <p className="dashboard-item">
-                    <Link className="dashboard-item-link" to="/review" >
-                        Review
-                    </Link>
-                </p>
+                {/* Normal User Menu  */}
+                <div>
+                    <p className="dashboard-item">
+                        {
+                            !isAdmin ? <Link className="dashboard-item-link" to="/pay" >
+                                Pay
+                            </Link> : ''
+                        }
+                    </p>
+                    <p className="dashboard-item">
+                        {
+                            !isAdmin ? <Link className="dashboard-item-link" to="/my-orders" >
+                                My Orders
+                            </Link> : ''
+                        }
+                    </p>
+                    <p className="dashboard-item">
+                        {
+                            !isAdmin ? <Link className="dashboard-item-link" to="/review" >
+                                Review
+                            </Link> : ''
+                        }
+                    </p>
+                </div>
 
-                <p className="dashboard-item">
-                    <Link className="dashboard-item-link" to="/add-product" >
-                        Add A Product
-                    </Link>
-                </p>
-                <p className="dashboard-item">
-                    <Link className="dashboard-item-link" to="/manage-products" >
-                        Manage Products
-                    </Link>
-                </p>
-                <p className="dashboard-item">
-                    <Link className="dashboard-item-link" to="/manage-all-orders" >
-                        Manage All Orders
-                    </Link>
-                </p>
-                <p className="dashboard-item">
-                    <Link className="dashboard-item-link" to="/make-admin" >
-                        Make An Admin
-                    </Link>
-                </p>
+                {/* Admin User Menu  */}
+                <div>
+                    <p className="dashboard-item">
+                        {
+                            isAdmin ? <Link className="dashboard-item-link" to="/add-product" >
+                                Add A Product
+                            </Link> : ''
+                        }
+                    </p>
+                    <p className="dashboard-item">
+                        {
+                            isAdmin ? <Link className="dashboard-item-link" to="/manage-products" >
+                                Manage Products
+                            </Link> : ''
+                        }
+                    </p>
+                    <p className="dashboard-item">
+                        {
+                            isAdmin ? <Link className="dashboard-item-link" to="/manage-all-orders" >
+                                Manage All Orders
+                            </Link> : ''
+                        }
+                    </p>
+                    <p className="dashboard-item">
+                        {
+                            isAdmin ? <Link className="dashboard-item-link" to="/make-admin" >
+                                Make An Admin
+                            </Link> : ''
+                        }
+                    </p>
+                </div>
 
+                {/* Logout Button  */}
                 <div className="border-top mt-3 pt-3">
                     <button className="btn-1">Logout</button>
                 </div>
             </div>
+
+            {/* Main Section  */}
             <div className="col-9 container pt-5">
                 <div><img src={userIcon} alt="" /></div>
                 <div>
-                    <h3>"Username Here"</h3>
-                    <p>Email: "Email Here"</p>
+                    <h3>"{user.displayName}"</h3>
+                    <p>Email: "{user.email}"</p>
                 </div>
             </div>
 
