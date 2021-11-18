461bab2f6f430ce426aea921541976314ffa83cb
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-19 02:19:33 +0600

    Warning fixes

--- src/pages/dashboard/MakeAdmin/MakeAdmin.js
+++ src/pages/dashboard/MakeAdmin/MakeAdmin.js
@@ -31,9 +31,9 @@ const MakeAdmin = () => {
         <div className="container my-5">
             <h1 className="txt-primary pb-3">MAKE AN ADMIN</h1>
             <h4 className="text-success">Give an email address to make an admin</h4>
-            <div class="form-floating mb-3 w-50 mx-auto">
-                <input type="email" ref={adminEmailRef} class="form-control" id="floatingInput" placeholder=" " />
-                <label for="floatingInput">Admin Email</label>
+            <div className="form-floating mb-3 w-50 mx-auto">
+                <input type="email" ref={adminEmailRef} className="form-control" id="floatingInput" placeholder=" " />
+                <label htmlFor="floatingInput">Admin Email</label>
             </div>
             <div><button className="btn-1 py-2" onClick={handelMakeAdmin} >Make Admin</button></div>
         </div>
