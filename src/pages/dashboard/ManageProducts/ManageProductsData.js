4d51bc4a4c1e979c5a99d7695dae77b2b99a03d6
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-19 01:35:59 +0600

    Product deleting process complete

--- src/pages/dashboard/ManageProducts/ManageProductsData.js
+++ src/pages/dashboard/ManageProducts/ManageProductsData.js
@@ -1,4 +1,5 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import useAuth from '../../../hooks/useAuth';
 
 const ManageProductsData = (props) => {
@@ -6,8 +7,13 @@ const ManageProductsData = (props) => {
     const statusRef = useRef();
     const { user } = useAuth();
 
-    // Handel Update Method 
-    const handelUpdate = () => {
+    // Modal Controls 
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    // Handle Update Method 
+    const handleUpdate = () => {
         const url = `http://localhost:7007/updateProduct?email=${user.email}`;
         const idToken = localStorage.getItem('idToken');
         const newCar = { ...props.value };
@@ -29,6 +35,29 @@ const ManageProductsData = (props) => {
             })
     }
 
+    // Handle Delete Method 
+    const handleDelete = () => {
+        const url = `http://localhost:7007/deleteProduct?email=${user.email}`;
+        const idToken = localStorage.getItem('idToken');
+        const car = { ...props.value };
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${idToken}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(car)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.deletedCount === 1) {
+                    handleClose();
+                }
+            })
+
+    }
+
+
     return (
         <tr>
             <td>{name}</td>
@@ -45,8 +74,32 @@ const ManageProductsData = (props) => {
                 </select>
             </td>
             <td>
-                <button onClick={handelUpdate} className="btn btn-success me-2" >Update</button>
-                <button className="btn btn-danger" >Delete</button>
+                <button onClick={handleUpdate} className="btn btn-success me-2" >Update</button>
+                {/* Button trigger modal  */}
+                <Button variant="danger" onClick={handleShow}>
+                    Delete
+                </Button>
+
+                <Modal
+                    show={show}
+                    onHide={handleClose}
+                    backdrop="static"
+                    keyboard={false}
+                    centered
+                >
+                    <Modal.Header closeButton>
+                        <Modal.Title>Modal title</Modal.Title>
+                    </Modal.Header>
+                    <Modal.Body>
+                        Delete operation will premanently delete this product from database. If you really want to delete this product then click Confirm Delete buton again, else Cancel.
+                    </Modal.Body>
+                    <Modal.Footer>
+                        <Button variant="secondary" onClick={handleClose}>
+                            Close
+                        </Button>
+                        <Button variant="danger" onClick={handleDelete}>Confirm Delete</Button>
+                    </Modal.Footer>
+                </Modal>
             </td>
         </tr>
     );
