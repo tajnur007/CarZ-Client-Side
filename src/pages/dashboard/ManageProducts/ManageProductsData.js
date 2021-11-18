dffb744664b7d69c8b446a19bc4720c31a1c8036
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-18 20:33:01 +0600

    Product update process complete

--- src/pages/dashboard/ManageProducts/ManageProductsData.js
+++ src/pages/dashboard/ManageProducts/ManageProductsData.js
@@ -1,8 +1,33 @@
 import React, { useRef } from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const ManageProductsData = (props) => {
     const { name, status } = props.value;
     const statusRef = useRef();
+    const { user } = useAuth();
+
+    // Handel Update Method 
+    const handelUpdate = () => {
+        const url = `http://localhost:7007/updateProduct?email=${user.email}`;
+        const idToken = localStorage.getItem('idToken');
+        const newCar = { ...props.value };
+        newCar.status = statusRef.current.value;
+        console.log(newCar);
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${idToken}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newCar)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.modifiedCount === 1) {
+                    alert('Product update successful');
+                }
+            })
+    }
 
     return (
         <tr>
@@ -20,7 +45,7 @@ const ManageProductsData = (props) => {
                 </select>
             </td>
             <td>
-                <button className="btn btn-success me-2" >Update</button>
+                <button onClick={handelUpdate} className="btn btn-success me-2" >Update</button>
                 <button className="btn btn-danger" >Delete</button>
             </td>
         </tr>
