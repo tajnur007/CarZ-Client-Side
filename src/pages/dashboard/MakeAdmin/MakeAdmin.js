a58781b574a0686e3e81f4077eb61fca9f8bbfb9
Kazi Tajnur Islam <tajnur007@gmail.com>
2021-11-18 23:58:43 +0600

    Making an admin process complete

--- src/pages/dashboard/MakeAdmin/MakeAdmin.js
+++ src/pages/dashboard/MakeAdmin/MakeAdmin.js
@@ -1,15 +1,41 @@
-import React from 'react';
+import React, { useRef } from 'react';
+import useAuth from '../../../hooks/useAuth';
 
 const MakeAdmin = () => {
+    const adminEmailRef = useRef();
+    const { user } = useAuth();
+
+    // Handel Make Admin Method 
+    const handelMakeAdmin = () => {
+        const url = `http://localhost:7007/makeAdmin?email=${user.email}`;
+        const idToken = localStorage.getItem('idToken');
+        const newAdminEmail = { email: `${adminEmailRef.current.value}` };
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'authorization': `Bearer ${idToken}`,
+                'content-type': 'application/json'
+            },
+            body: JSON.stringify(newAdminEmail)
+        })
+            .then(resp => resp.json())
+            .then(data => {
+                if (data.modifiedCount === 1) {
+                    alert('Admin added successfully.');
+                    adminEmailRef.current.value = '';
+                }
+            })
+    }
+
     return (
         <div className="container my-5">
             <h1 className="txt-primary pb-3">MAKE AN ADMIN</h1>
             <h4 className="text-success">Give an email address to make an admin</h4>
             <div class="form-floating mb-3 w-50 mx-auto">
-                <input type="email" class="form-control" id="floatingInput" placeholder=" " />
+                <input type="email" ref={adminEmailRef} class="form-control" id="floatingInput" placeholder=" " />
                 <label for="floatingInput">Admin Email</label>
             </div>
-            <div><button className="btn-1 py-2">Make Admin</button></div>
+            <div><button className="btn-1 py-2" onClick={handelMakeAdmin} >Make Admin</button></div>
         </div>
     );
 };
